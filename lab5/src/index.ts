9339d14a238ef65617a3101cb4e27ca7dd3e1a9d
lenam2509 <levietnam2509@gmail.com>
2023-07-26 14:25:26 +0700

    lab6

--- lab5/src/index.ts
+++ lab5/src/index.ts
@@ -1,1 +1,32 @@
-console.log('hello lab 5');
+import axios from 'axios';
+import { User } from './users';
+
+const url = 'http://localhost:3000/users/';
+
+
+// axios.post(url, {
+//     name: 'Le Viet Nam',
+//     age: 23,
+//     address: '123 HO CHI MINH'
+// });
+
+axios.put(url+'2', {
+    name: 'Le Viet Nam ',
+    age: Math.random(),
+    address: '123 HO CHI MINH'
+})
+
+const user1 = new User({
+    name: 'Le Viet Nam',
+    age: 23,
+    address: '123 HO CHI MINH'
+})
+
+user1.set({name: 'Le Viet Nam 2'});
+console.log(user1.get('name'));
+
+user1.on("update", () => {
+    console.log("User data has been updated!");
+});
+  
+user1.trigger("update")
