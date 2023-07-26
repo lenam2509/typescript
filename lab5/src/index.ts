53c55786e66b09b8cb30d12099f280ad298ca3d6
lenam2509 <levietnam2509@gmail.com>
2023-07-26 14:40:30 +0700

    lab6

--- lab5/src/index.ts
+++ lab5/src/index.ts
@@ -10,23 +10,28 @@ const url = 'http://localhost:3000/users/';
 //     address: '123 HO CHI MINH'
 // });
 
-axios.put(url+'2', {
-    name: 'Le Viet Nam ',
-    age: Math.random(),
-    address: '123 HO CHI MINH'
-})
+// axios.put(url+'2', {
+//     name: 'Le Viet Nam ',
+//     age: Math.random(),
+//     address: '123 HO CHI MINH'
+// // })
 
-const user1 = new User({
-    name: 'Le Viet Nam',
-    age: 23,
-    address: '123 HO CHI MINH'
-})
+// const user1 = new User({
+//     name: 'Le Viet Nam',
+//     age: 23,
+//     address: '123 HO CHI MINH'
+// })
 
-user1.set({name: 'Le Viet Nam 2'});
-console.log(user1.get('name'));
+// console.log(user1);
 
-user1.on("update", () => {
-    console.log("User data has been updated!");
-});
+
+// user1.set({name: 'Le Viet Nam 2'});
+// console.log(user1.get('name'));
+
+// user1.on("update", () => {
+//     console.log("User data has been updated!");
+// });
   
-user1.trigger("update")
+// user1.trigger("update")
+
+
