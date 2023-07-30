ed1827f2785792e05e1465c6f7f0051d11d71383
Le Viet Nam <levietnam2509@gmail.com>
2023-07-30 14:42:03 +0700

    lab7

--- lab5/src/index.ts
+++ lab5/src/index.ts
@@ -4,40 +4,6 @@ import { User, UserProps } from './users';
 const url = 'http://localhost:3000/users/';
 const user_url = 'http://localhost:3000/users/1';
 
-// axios.post(url, {
-//     name: 'Le Viet Nam',
-//     age: Math.round(Math.random() * 100),
-//     address: '123 HO CHI MINH',
-//     job: 'Student',
-//     phone: '123456789',
-//     email: 'levietnam@gmail'
-// });
-
-// axios.put(url+'2', {
-//     name: 'Le Viet Nam ',
-//     age: Math.random(),
-//     address: '123 HO CHI MINH'
-// // })
-
-// const user1 = new User({
-//     name: 'Le Viet Nam',
-//     age: 23,
-//     address: '123 HO CHI MINH'
-// })
-
-// console.log(user1);
-
-
-// user1.set({name: 'Le Viet Nam 2'});
-// console.log(user1.get('name'));
-
-// user1.on("update", () => {
-//     console.log("User data has been updated!");
-// });
-  
-// user1.trigger("update")
-
-
 axios.get(user_url).then((res) => {
     const users = res.data;
     console.log(users);
@@ -53,17 +19,89 @@ axios.get(user_url).then((res) => {
          </div>
      `
     document.querySelector('.info')!.innerHTML = html;
-    // users.forEach((user: UserProps) => {
-    //     const user1 = new User(user);
-    //     const html = `
-    //     <div class="info-box">
-    //         <h1>${user1.get('name')}</h1>
-    //         <h2>${user1.get('job')}</h2>
-    //         <p> <i class="fa-solid fa-phone"></i>${user1.get('phone')}</p>
-    //         <p><i class="fa-solid fa-envelope"></i>${user1.get('email')}</p>
-    //         <p><i class="fa-solid fa-location-dot"></i>${user1.get('address')}</p>
-    //     </div>
-    // `
-    // document.querySelector('.info')!.innerHTML = html;
-    // })
 });
+
+axios.get(user_url).then((res) => {
+    const users = res.data;
+    // load data to form
+    const form_update = document.querySelector('.form_update')! as HTMLFormElement;
+    form_update.name.value = users.name;
+    form_update.job.value = users.job;
+    form_update.phone.value = users.phone;
+    form_update.email.value = users.email;
+    form_update.address.value = users.address;
+});
+
+
+
+function handleSubmit(event: Event) {
+    event.preventDefault();
+    const form = document.querySelector('.form')! as HTMLFormElement;
+    const formData = new FormData(form);
+    const name = formData.get('name') as string;
+    const job = formData.get('job') as string;
+    const phone = formData.get('phone') as unknown as number;
+    const email = formData.get('email') as string;
+    const address = formData.get('address') as string;
+
+    axios.post(url, {
+        name: name,
+        job: job,
+        phone: parseInt(phone.toString()),
+        email: email,
+        address: address,
+    })
+        .then(function (response) {
+            console.log(response);
+            if (response.status === 201) {
+                alert('Thêm thành công');
+                window.location.reload();
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    
+
+    
+
+}
+
+function handleUpdate(event: Event) {
+    event.preventDefault();
+    const form_update = document.querySelector('.form_update')! as HTMLFormElement;
+    const formData = new FormData(form_update);
+    const name = formData.get('name') as string;
+    const job = formData.get('job') as string;
+    const phone = formData.get('phone') as unknown as number;
+    const email = formData.get('email') as string;
+    const address = formData.get('address') as string;
+
+    axios.put(user_url, {
+        name: name,
+        job: job,
+        phone: parseInt(phone.toString()),
+        email: email,
+        address: address,
+    })
+        .then(function (response) {
+            console.log(response);
+            if (response.status === 200) {
+                alert('Cập nhật thành công');
+                window.location.reload();
+            }
+        }
+        )
+        .catch(function (error) {
+            console.log(error);
+        }
+        );
+}
+
+
+const form = document.querySelector('.form')!;
+form.addEventListener('submit', handleSubmit);
+
+const form_update = document.querySelector('.form_update')!;
+form_update.addEventListener('submit', handleUpdate);
+
