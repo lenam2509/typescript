36304f0c0d626fe9ada54cf9d965da3f58bc81aa
lenam2509 <levietnam2509@gmail.com>
2023-07-31 14:15:35 +0700

    comit

--- ASS/src/admin.ts
+++ ASS/src/admin.ts
@@ -1,13 +1,32 @@
 import axios from "axios";
 
 
-// fetch('http://localhost:3000/milks').then(response => {
-//     console.log(response.json());
-// });
+
 
 
 axios.get('http://localhost:3000/milks').then(response => {
-    console.log(response.data);
+   const milks = response.data;
+   
+    milks.forEach((milk: { id: number; name: string; price: number; thumbnail:string }) => {
+        const html = `
+        <tr>
+        <th scope="row">${milks.length}</th>
+        <td>${milk.id}</td>
+        <td>${milk.name}</td>
+        <td>${milk.price}</td>
+        <td>
+            <img src="${milk.thumbnail}}"
+                width="50px" height="50px" alt="">
+        </td>
+        <td>
+            <p>
+                <button class="btn btn-warning">Edit</button>
+                <button class="btn btn-danger">Delete</button>
+            </p>
+        </td>
+    </tr>
+        `;
+        document.querySelector('#milkList').innerHTML += html;
+    });
 });
 
-// console.log('Starting admin');
