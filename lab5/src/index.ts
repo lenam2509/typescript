abb0617214c20d36ee54655b43cc4a3d7696d115
lenam2509 <levietnam2509@gmail.com>
2023-07-31 13:30:24 +0700

    commit

--- lab5/src/index.ts
+++ lab5/src/index.ts
@@ -1,8 +1,20 @@
 import axios from 'axios';
 import { User, UserProps } from './users';
 
+let id:number = 1
 const url = 'http://localhost:3000/users/';
-const user_url = 'http://localhost:3000/users/1';
+const user_url = `http://localhost:3000/users/${id}`;
+
+
+axios.get(url).then((res) => {
+    const users = res.data;
+    users.forEach((user: any) => {
+        const html = `
+          <option value="${user.id}">${user.name}</option>
+        `
+        document.querySelector('#user_id')!.innerHTML += html;
+    });
+});
 
 axios.get(user_url).then((res) => {
     const users = res.data;
@@ -105,3 +117,31 @@ form.addEventListener('submit', handleSubmit);
 const form_update = document.querySelector('.form_update')!;
 form_update.addEventListener('submit', handleUpdate);
 
+const select = document.querySelector('#user_id')! as HTMLSelectElement;
+select.addEventListener('change', (e) => {
+        e.preventDefault();
+        id = parseInt(select.value);
+        console.log(id);
+    
+        axios.get(`http://localhost:3000/users/${id}`).then((res) => {
+            const users = res.data;
+            console.log(users);
+            const form_update = document.querySelector('.form_update')! as HTMLFormElement;
+            form_update.name.value = users.name;
+            form_update.job.value = users.job;
+            form_update.phone.value = users.phone;
+            form_update.email.value = users.email;
+            form_update.address.value = users.address;
+            const user1 = new User(users);
+            const html = `
+                <div class="info-box">
+                   <h1>${user1.get('name')}</h1>
+                   <h2>${user1.get('job')}</h2>
+                     <p> <i class="fa-solid fa-phone"></i>${user1.get('phone')}</p>
+                     <p><i class="fa-solid fa-envelope"></i>${user1.get('email')}</p>
+                     <p><i class="fa-solid fa-location-dot"></i>${user1.get('address')}</p>
+                 </div>
+             `
+            document.querySelector('.info')!.innerHTML = html;
+        });
+});
